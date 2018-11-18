65517effd7200d81a936c879dcf19563a311f9c0
Blake Zimmerman <blake.zimmerman@icloud.com>
2018-11-18 10:51:04 -0500

    Add transp.c

---
+++ transp.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+
+// Check if string input is power of 2
+int is_power_of_two(char *val)
+{
+  int num = atoi(val);
+  return (ceil(log2(num)) == floor(log2(num)));
+}
+
+int main(int argc, char **argv)
+{
+  // Verify command line arguments
+  if (argc != 3)
+  {
+    fprintf(stderr, "Wrong number of command-line arguments\n");
+    fprintf(stderr, "Usage: %s <matrix_width> <block_width>\n", argv[0]);
+    return -1;
+  }
+
+  // Validate args
+  if (!is_power_of_two(argv[1]))
+  {
+    fprintf(stderr, "matrix_width must be a power of 2\n");
+    return -1;
+  }
+  if (!is_power_of_two(argv[2]))
+  {
+    fprintf(stderr, "block_width must be a power of 2\n");
+    return -1;
+  }
+
+  int matrix_width = atoi(argv[1]);
+  int block_width = atoi(argv[2]);
+
+  struct timeval tvalBefore, tvalAfter;
+
+  // Allocate matrices
+  float *input = (float *)malloc(matrix_width * matrix_width * sizeof(float));
+  float *output = (float *)malloc(matrix_width * matrix_width * sizeof(float));
+
+  // Seed pseudo-random number generator
+  srand((unsigned int)time(NULL));
+
+  // Fill matrix with random floats (-100 - 100)
+  for (int i = 0; i < matrix_width * matrix_width; i++)
+  {
+    input[i] = ((float)rand()) / RAND_MAX * 200 - 100;
+  }
+
+  // Get starting time
+  gettimeofday(&tvalBefore, NULL);
+
+  // Peform Block-Transpose
+  for (int i = 0; i < matrix_width; i += block_width)
+  {
+    for (int j = 0; j < matrix_width; j += block_width)
+    {
+      for (int k = i; k < i + block_width; ++k)
+      {
+        for (int l = j; l < j + block_width; ++l)
+        {
+          output[k + l * matrix_width] = input[l + k * matrix_width];
+        }
+      }
+    }
+  }
+
+  // Get ending time
+  gettimeofday(&tvalAfter, NULL);
+
+  // Report execution time
+  printf("Time in microseconds: %ld microseconds\n",
+         ((tvalAfter.tv_sec - tvalBefore.tv_sec) * 1000000L + tvalAfter.tv_usec) - tvalBefore.tv_usec);
+
+  // Clean up and end program
+  free(input);
+  free(output);
+  return 0;
+}
